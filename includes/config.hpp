250eef9ba5e0dca2836a8aab51cb378050c5726f
maiadegraaf <68693691+maiadegraaf@users.noreply.github.com>
2022-11-03 13:49:44 +0100

    edit config.cpp config.hpp

--- includes/config.hpp
+++ includes/config.hpp
@@ -2,7 +2,7 @@
 #define CONFIG_H
 #include <iostream>
 #include <string>
-#include <map>
+#include <vector>
 #include "location.hpp"
  
 // Class definition 
@@ -10,32 +10,32 @@ class config
 {
 private: 
 	int _address;
-	std::map _server_name;
+	std::vector<std::string> _server_name;
 	std::string _root;
 	unsigned long long _max_size;
-	location _location;
+	std::vector<location> _location;
 	std::string _cgi;
  
 public: 
 // Constructor 
 	config(); 
 	config( const config &rhs); 
-	config(int newAddress, std::map newServer_name, std::string newRoot, unsigned long long newMax_size, location newLocation, std::string newCgi);
+	config(int newAddress, std::vector<std::string> newServer_name, std::string newRoot, unsigned long long newMax_size, location newLocation, std::string newCgi);
 	~config(); 
 	config& operator=( const config &rhs); 
 // Getters 
 	int getAddress(); 
-	std::map getServer_name(); 
+	std::vector<std::string> getServer_name();
 	std::string getRoot(); 
-	unsigned long long getMax_size(); 
-	location getLocation(); 
+	unsigned long long getMax_size();
+    std::vector<location> getLocation();
 	std::string getCgi(); 
 // Setters 
 	void setAddress(int _address); 
-	void setServer_name(std::map _server_name); 
+	void setServer_name(std::vector<std::string> _server_name);
 	void setRoot(std::string _root); 
 	void setMax_size(unsigned long long _max_size); 
-	void setLocation(location _location); 
+	void setLocation(std::vector<location> _location);
 	void setCgi(std::string _cgi); 
 // Output 
 	void output(); 
--- src/config.cpp
+++ src/config.cpp
@@ -10,8 +10,8 @@ config::config( const config& rhs)
 {
 	*this = rhs;
 }
- 
-config::config(int newAddress, std::map newServer_name, std::string newRoot, unsigned long long newMax_size, location newLocation, std::string newCgi) 
+
+config::config(int newAddress, std::vector<std::string> newServer_name, std::string newRoot, unsigned long long newMax_size, location newLocation, std::string newCgi)
 	: _address(newAddress), _server_name(newServer_name), _root(newRoot), _max_size(newMax_size), _location(newLocation), _cgi(newCgi)
 {
 
@@ -29,27 +29,28 @@ config&	config::operator=( const config& rhs )
 
 // Getters 
 int config::getAddress() { return _address; }
-//std::map config::getServer_name() { return _server_name; }
+//std::vector<std::string> config::getServer_name() { return _server_name; }
 std::string config::getRoot() { return _root; }
 unsigned long long config::getMax_size() { return _max_size; }
-location config::getLocation() { return _location; }
+std::vector<location> config::getLocation() { return _location; }
 std::string config::getCgi() { return _cgi; }
  
 // Setters 
 void config::setAddress(int newAddress) { _address = newAddress; }
-void config::setServer_name(std::map newServer_name) { _server_name = newServer_name; }
+void config::setServer_name(std::vector<std::string> newServer_name) { _server_name = newServer_name; }
 void config::setRoot(std::string newRoot) { _root = newRoot; }
 void config::setMax_size(unsigned long long newMax_size) { _max_size = newMax_size; }
-void config::setLocation(location newLocation) { _location = newLocation; }
+void config::setLocation(std::vector<location> newLocation) { _location = newLocation; }
 void config::setCgi(std::string newCgi) { _cgi = newCgi; }
  
 // Output
 void config::output()
 {
   std::cout << "address : " << _address << std::endl; 
-  std::cout << "server_name : " << _server_name << std::endl; 
+//  std::cout << "server_name : " << _server_name << std::endl;
   std::cout << "root : " << _root << std::endl; 
-  std::cout << "max_size : " << _max_size << std::endl; 
+  std::cout << "max_size : " << _max_size << std::endl;
+//  _location.output();
   std::cout << "cgi : " << _cgi << std::endl;
 }
  
