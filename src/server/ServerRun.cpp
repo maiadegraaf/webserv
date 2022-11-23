f6c87fc14d3369122700f98740ef855db82a9cc6
Storm De quaij <sde-quai@f1r4s1.codam.nl>
2022-11-23 16:56:40 +0100

    dikke bak ellende

--- src/server/ServerRun.cpp
+++ src/server/ServerRun.cpp
@@ -3,70 +3,79 @@
 void Server::run()
 {
 	creatingKqueue();
+//	int i = 0;
 	while(1) {
-		newEvent();
 		loopEvent();
-	}
-}
-
-void Server::newEvent()
-{
-	_new_events = kevent(_kq, NULL, 0, _event, 1, NULL);
-	if (_new_events == -1)
-	{
-		perror("kevent");
-		exit(1);
+//		i++;
+//		i = i % 2;
 	}
 }
 
 void Server::creatingKqueue()
 {
+	struct kevent			_change_event[2];
 	cout << "waiting kqueue..." << endl;
 	_kq = kqueue();
-	EV_SET(_change_event, _fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
-	if (kevent(_kq, _change_event, 1, NULL, 0, NULL) == -1)
+	EV_SET(&_change_event[0], _fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
+	EV_SET(&_change_event[1], _fd, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, 0);
+	if (kevent(_kq, _change_event, 2, NULL, 0, NULL) == -1)
 	{
 		perror("kevent");
 		exit(1);
 	}
 }
 
+//
+//void Server::newEvent()
+//{
+//	_new_events = kevent(_kq, NULL, 0, _event, 2, NULL);
+//	if (_new_events == -1)
+//	{
+//		perror("kevent");
+//		exit(1);
+//	}
+//}
+
 void Server::loopEvent()
 {
-	for (int i = 0; _new_events > i; i++) {
-		printf("amount of new events: %d\n", _new_events);
-		_event_fd = _event[i].ident;
+	struct kevent			_event[2];
 
-		// When the client disconnects an EOF is sent. By closing the file
-		// descriptor the _event is automatically removed from the kqueue.
-		if (_event[i].flags & EV_EOF) {
-			printf("Client has disconnected\n");
-			close(_event_fd);
-			// remove client
+//		printf("amount of new events: %d\n", _new_events);
+	_event_fd = _event[0].ident;
+
+	// When the client disconnects an EOF is sent. By closing the file
+	// descriptor the _event is automatically removed from the kqueue.
+	if (_event[0].flags & EV_EOF) {
+		printf("Client has disconnected\n");
+		close(_event_fd);
+//		 delete client class;
+		// remove client
+	}
+		// If the new _event's file descriptor is the same as the listening
+		// socket's file descriptor, we are sure that a new client wants
+		// to connect to our socket.
+	else if (_event_fd == _fd) {
+		printf("New connection coming in...\n");
+
+		// Incoming socket connection on the listening socket.
+		// Create a new socket for the actual connection to client.
+		_newFd = accept(_event_fd, (struct sockaddr *) &_client_addr, (socklen_t * ) & _len);
+		if (_newFd == -1) {
+			perror("Accept socket error");
 		}
-			// If the new _event's file descriptor is the same as the listening
-			// socket's file descriptor, we are sure that a new client wants 
-			// to connect to our socket.
-		else if (_event_fd == _fd) {
-			printf("New connection coming in...\n");
+		cout << "Accepted new client fd" << endl;
 
-			// Incoming socket connection on the listening socket.
-			// Create a new socket for the actual connection to client.
-			_newFd = accept(_event_fd, (struct sockaddr *) &_client_addr, (socklen_t * ) & _len);
-			if (_newFd == -1) {
-				perror("Accept socket error");
-			}
-
-			// Put this new socket connection also as a 'filter' _event
-			// to watch in kqueue, so we can now watch for events on this
-			// new socket.
-			EV_SET(_change_event, _newFd, EVFILT_READ, EV_ADD, 0, 0, NULL);
-			if (kevent(_kq, _change_event, 1, NULL, 0, NULL) < 0) {
-				perror("kevent error");
-			}
-		} else if (_event[i].filter & EVFILT_READ) {
-			clientRequest();
+		// Put this new socket connection also as a 'filter' _event
+		// to watch in kqueue, so we can now watch for events on this
+		// new socket.
+		EV_SET(&_event[0], _newFd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+		EV_SET(&_event[1], _newFd, EVFILT_WRITE, EV_ADD, 0, 0, NULL);
+		cout << "EV_SET event" << endl;
+		if (kevent(_kq, NULL, 0, _event, 2, NULL) < 0) {
+			perror("kevent error");
 		}
+	} else if (_event[0].filter & EVFILT_READ) {
+		clientRequest();
 	}
 }
 
